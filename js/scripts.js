801ea649279199a708357ded64f8967748a48d31
StacyChebet <staceychebet23@gmail.com>
2018-09-11 10:04:31 +0300

    add the initial scripts.js page

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var add= add(number1, number2) {
+2
+return number1 + number2;
+3
+};
+4
+var number1 = parseInt(prompt("Enter a number:"));
+5
+var number2 = parseInt(prompt("Enter another number:"));
+6
+alert(add(number1, number2);
+7
